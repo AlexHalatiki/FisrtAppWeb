59ae4387eb2b4f6b806f57d0e2d1bc3c2caf471a
Alex Halatiki <alexhvicente1@gmail.com>
2022-09-13 15:07:02 -0300

    First Model-Controller-View - Deparment

---
+++ SalesWebMvc/Controllers/DepartamentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+        }
+    }
+}
